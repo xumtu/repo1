d3396691e98a64f5a66e73a23a1659020a88ed21
zhangsan <zhangsan@qq.com>
2019-03-21 18:54:25 +0800

    gitTest01

--- gitTest01/src/com/itheima/git/FirstClass.java
+++ gitTest01/src/com/itheima/git/FirstClass.java
@@ -3,5 +3,10 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello word");
+
+    }
+
+    public void test(){
+        System.out.println("来了，老弟");
     }
 }
